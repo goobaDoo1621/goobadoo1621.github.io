3b8b33cd9e388c7c0c65832a4a65470bcba38730
Matt <143457558+goobaDoo1621@users.noreply.github.com>
2025-12-01 16:35:17 -0600

    Add files via upload
    senior speech bingo

---
+++ srsp-bingo/bingo.js
@@ -0,0 +1,81 @@
+const bingoCards = [
+    "Tour team shoutout",
+    "\"We had a 1am call time\"",
+    "\"MIZ\"",
+    "Tells a joke and nobody laughs",
+    "Neck innuendo",
+    "Grace the robot",
+    "Section slogan",
+    "Bad word",
+    "Mini Mizzou story",
+    "Says something that obviously pisses off the directors",
+    "Heyyy maybe don\'t say this to a large group of people",
+    "Breathes into the mic really loudly",
+    "Significant other mention",
+    "\"I started on the waitlist\"",
+    "Really awkward intro",
+    "Bicentennial parade",
+    "\"I almost quit...\"",
+    "KKΨ or TBS shoutout",
+    "Section diss",
+    "Gasparilla Bowl or Armed Forces Bowl",
+    "Really long speech",
+    "Cries",
+    "Spot the dog",
+    "Band bunch",
+    "I\'ve never seen this person in my life",
+    "\"To keep it short...\" (it was not, in fact, short)",
+    "Mentions coming from a different state",
+    "Hugs a director",
+    "Tipperary",
+    "Mentions weather of a football game",
+    "\"Go tigers\"",
+    "Marching 4 to 5 through NYC"
+];
+
+
+function randomSubset(arr, k) {
+    const a = arr.slice(); // copy
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a.slice(0, k);
+}
+
+function generateBoard() {
+    let bb = randomSubset(bingoCards, bSize);
+    for(let i = 0; i < bSize; i++) {
+        board[i].textContent = bb[i];
+        board[i].style.setProperty("background-color", "white")
+    }
+    board[12].textContent = "Free";
+    board[12].style.setProperty("background-color", "gold");
+}
+
+function tileFunct(event) {
+    if(event.type === "mouseover" && getComputedStyle(this).backgroundColor !== "rgb(255, 215, 0)") {
+        this.style.setProperty("background-color", "lightgreen");
+    }
+
+    if(event.type === "mouseout" && getComputedStyle(this).backgroundColor !== "rgb(255, 215, 0)") {
+        this.style.setProperty("background-color", "white");
+    }
+
+    if(event.type === "click" && getComputedStyle(this).backgroundColor !== "rgb(255, 215, 0)") {
+        this.style.setProperty("background-color", "gold");
+    }
+}
+
+
+const b = document.querySelector(".board").children;
+const bSize = 25;
+let board = Array.from(b);
+generateBoard();
+
+
+for(let i = 0; i < bSize; i++) {
+    board[i].addEventListener("click", tileFunct);
+    board[i].addEventListener("mouseover", tileFunct);
+    board[i].addEventListener("mouseout", tileFunct);
+}
